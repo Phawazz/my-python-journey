1624f5a73f19fda814c969ee2988264b181b6d3f
Phawazz <fawazbello2003@gmail.com>
2026-01-08 23:57:24 +0100

    feat: Implement basic gradient descent algorithm from scratch

--- mse_loss.py
+++ mse_loss.py
@@ -13,7 +13,38 @@ def calculate_mse(y_true, y_pred): # calculating mean squared error manually
 # Testing with a dummy data
 
 actual_scores = [65, 70, 78, 90]
-predicted_scores = [2.5, 0.0, 2.1, 7.8]
+predicted_scores = [45, 50, 61, 78]
 
 mse = calculate_mse(actual_scores, predicted_scores)
 print(f"The mean squared error is {mse}")
+
+
+# Gradient descent
+
+import numpy as np
+
+x = np.array([65, 70, 78, 90])
+y = np.array([45, 50, 61, 78])
+
+#Hyperparameters: Initial targets
+m = 0
+c = 0
+learning_rate = 0.0001
+epochs = 1000
+
+n = len(x)
+
+for i in range(epochs):
+    y_pred = m * x + c
+    
+    d_m = (-2/n) * sum(x * y - y_pred)
+    d_c = (-2/n) * sum(y - y_pred)
+    
+    m = m - learning_rate * d_m
+    c = c - learning_rate * d_c
+    
+    if i % 100 == 0:
+        print(f"Epoch {i}: m={m:.4f}, c={c:.4f}")
+
+print(f"\nFinal Model: y = {m:.4f}x + {c:.4f}")
+
